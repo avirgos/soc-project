0349cac0ec969086b6c2cd21f1bfbdd21add6b77
Antoine Virgos <antoine@virgos.fr>
2024-11-10 00:27:17 +0100

    Documentation Automatisation + splunk_alert_report.py, récupération des alertes High datant d'aujourd'hui

--- automatisation/splunk-alert-report/README.md
+++ automatisation/splunk-alert-report/README.md
@@ -8,3 +8,19 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+
+**⚠️ Dans le script Python ```splunk_alert_report.py```, vous devez compléter la valeur `<username>` et `<password>` avec vos identifiants Splunk pour pouvoir se connecter à l'instance. ⚠️**
+
+## Utilisation
+
+Exécutez ```splunk_alert_report.py``` :
+
+```bash
+python3 splunk_alert_report.py
+```
+
+Ce script est en charge de se connecter à l'instance Splunk et de récupérer toutes les données concernant les alertes de sévérité **"High"** ayant été déclenchées. Les données obtenues se situent dans le répertoire `splunk-alerts/`.
+
+Puis, le script Python réalise un tri pour n'obtenir que celles datant d'aujourd'hui.
+
+Au final, on obtient ces alertes au format Markdown dans un template Jinja.
--- automatisation/splunk-alert-report/splunk_alert_report.py
+++ automatisation/splunk-alert-report/splunk_alert_report.py
@@ -1,9 +1,10 @@
 import splunklib.client as client
 import json
-from jinja2 import Template
+from jinja2 import Environment
 from datetime import datetime
+import os
 
-# Connexion à Splunk
+# connect to Splunk instance
 service = client.connect(
     host='172.10.80.212',
     port='8089',
@@ -12,45 +13,69 @@ service = client.connect(
     password='<password>'
 )
 
-# Requête de recherche Splunk
-search_query = 'search index="alerts" event_title="*"'
+# Splunk search query to retrieve only "High" severity alerts
+search_query = 'search index="alerts" event.event_title="*" event.impact="high"'
+
+# convert ISO timestamp to formatted datetime
+def to_datetime(timestamp):
+    return datetime.fromisoformat(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
 try:
-    # Envoi de la requête de recherche à Splunk
+    # send the search query to Splunk
     search_results = service.jobs.oneshot(search_query, output_mode='json')
 
-    # Récupération des résultats de la recherche au format JSON
+    # retrieve search results in JSON format
     jsout = json.loads(search_results.read())
 
-    # Enregistrement des résultats dans un fichier JSON
+    # directory to store Splunk alerts
+    directory = "./splunk-alerts"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # save results to a JSON file
     json_formatted_str = json.dumps(jsout, indent=2)
 
-    with open('alerts-data.json', 'w') as f:
+    json_filename = os.path.join(directory, f'alerts-data-high-severity.json')
+    with open(json_filename, 'w') as f:
         f.write(json_formatted_str)
 
-    # Modèle de bulletin d'alerte en Markdown
+    # Markdown alert bulletin template
     template = """
-    {% for result in results %}
-    # Alerte : {{ result['event_title'] }}
-    Source : **{{ result['source'] }}**
-    Horodatage : {{ result['_time'] | to_datetime }}
-    Instance Splunk : {{ result['host'] }}
-    {% endfor %}
+{% for result in results %}
+# Alerte : {{ result['event.event_title'] }}
+Sévérité : **{{ result['event.impact'] }}**
+Description : {{ result['originQuery'] }}
+Horodatage : {{ result['_time'] | to_datetime }}
+{% endfor %}
     """
 
-    # Fonction pour convertir le timestamp ISO en datetime formaté
-    def to_datetime(timestamp):
-        return datetime.fromisoformat(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    # Jinja2 environment and filter
+    env = Environment()
+    env.filters['to_datetime'] = to_datetime
 
-    # Enregistrement du filtre pour Jinja2
-    Template.filters['to_datetime'] = to_datetime
+    # process results
+    filtered_results = []
+    today_date = datetime.now().strftime('%Y-%m-%d')
 
-    jinja_template = Template(template)
+    for result in jsout['results']:
+        raw_data = json.loads(result['_raw'])
+
+        # description of an alert
+        result['originQuery'] = raw_data['event']['originQuery']['description']
+
+        # convert "_time" to datetime object for filtering
+        event_time = datetime.fromisoformat(result['_time'].replace('Z', '+00:00'))
+        
+        # retrieve Splunk alerts from today
+        if event_time.date() == today_date:
+            filtered_results.append(result)
+
+    # create a model from the string template
+    jinja_template = env.from_string(template)
     
-    # Rendu du modèle avec les résultats
-    print(jinja_template.render(results=jsout['results']))
-
+    # render the template with the results
+    print(jinja_template.render(results=filtered_results))
 except Exception as e:
-    print(f"Une erreur est survenue : {e}")
+    print(f"⚠️ An error occurred : {e}")
 finally:
     service.logout()
